5359e93ba034cd0f305225031e793786462d899b
Cernean <ccernean@yahoo.com>
2023-02-26 12:34:22 -0500

    delete route complete

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-//DEPENDENCIES
+// DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
+
 
 // CONFIGURATION
 require('dotenv').config()
-
 const PORT = process.env.PORT
 const app = express()
 
@@ -11,8 +12,10 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+
 
 
 // ROUTES
