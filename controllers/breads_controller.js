978b89c2bc157b0f70838bc168fbc19c4dbc3fe9
Cernean <ccernean@yahoo.com>
2023-03-07 19:13:46 -0500

    BreadCrud Virtuals

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,48 +1,63 @@
 const express = require('express')
-//const { startSession } = require('../models/bread.js')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
+const Baker = require('../models/baker.js')
 
-// INDEX
+// Index:
 breads.get('/', (req, res) => {
-  Bread.find()
+  Baker.find()
+    .then(foundBakers => {
+      Bread.find()
       .then(foundBreads => {
           res.render('index', {
               breads: foundBreads,
+              bakers: foundBakers,
               title: 'Index Page'
           })
       })
+    })
 })
 
-// NEW
+//New route
 breads.get('/new', (req, res) => {
-  res.render('new')
+    Baker.find()
+        .then(foundBakers => {
+            res.render('new', {
+                bakers: foundBakers
+            })
+      })
 })
+
 
 // EDIT
 breads.get('/:id/edit', (req, res) => {
-  Bread.findById(req.params.id) 
-    .then(foundBread => { 
-      res.render('edit', {
-        bread: foundBread 
-      })
+  Baker.find()
+    .then(foundBakers => {
+        Bread.findById(req.params.id)
+          .then(foundBread => {
+            res.render('edit', {
+                bread: foundBread, 
+                bakers: foundBakers 
+            })
+          })
     })
 })
+
 
 // SHOW
 breads.get('/:id', (req, res) => {
   Bread.findById(req.params.id)
-    .then(foundBread => {
-      const breadsByJoey = Bread.getBakedByJoey();
-      console.log(breadsByJoey);
-      res.render('show', {
-        bread: foundBread
+      .populate('baker')
+      .then(foundBread => {
+        res.render('show', {
+            bread: foundBread
+        })
       })
-    })
-    .catch(err => {
-      res.send('404')
-    })
+      .catch(err => {
+        res.send('404')
+      })
 })
+
 
 // UPDATE
 breads.put('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -33,18 +33,22 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
   
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
-  
+  // breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+// bakers 
+const bakersController = require('./controllers/bakers_controller.js')
+app.use('/bakers', bakersController)
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
   
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
 
-// 404 Page
-app.get('*', (req, res) => {
-  res.send('404')
-})
 
